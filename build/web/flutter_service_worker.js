2e82925ecc1f571ec733d90c7032009a36c5c40c
kiran <kirancse.rymec@gmail.com>
2025-03-06 18:50:48 +0530

    Update10 Portfolio

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "a05fcf4b1813f7be6df66484937273ad",
-"index.html": "9d61a291d967e679a5583dfce991bfab",
-"/": "9d61a291d967e679a5583dfce991bfab",
+"flutter_bootstrap.js": "2361316b549448bc1aac864efb86b7ce",
+"index.html": "70bbfc98e804bf2eac64ea933844c695",
+"/": "70bbfc98e804bf2eac64ea933844c695",
 "main.dart.js": "7ce0faed70e963659b2df6d2b307c49c",
 "manifest.json": "0288a65fcfb6b8bff0a8711e4f9dea3d",
 "version.json": "009c9e65172e010890f7f65fde438006"};
